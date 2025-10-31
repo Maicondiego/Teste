beb85e8600b2769a7c8fc3833bb9972ae2b3423b
vidshort <147796365+Maicondiego@users.noreply.github.com>
2025-10-31 01:31:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,15 @@
-self.addEventListener('install',()=>{console.log('SW instalado');});
-self.addEventListener('activate',()=>{console.log('SW ativo');});
-self.addEventListener('notificationclick', event=>{
+// service-worker.js
+
+self.addEventListener('install', event => {
+  console.log('Service Worker instalado.');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker ativo.');
+});
+
+// Quando a notificação for clicada
+self.addEventListener('notificationclick', event => {
   event.notification.close();
-  const url = event.notification.data?.url || '/';
-  event.waitUntil(
-    clients.matchAll({type:'window', includeUncontrolled:true}).then(clientsArr=>{
-      for(const client of clientsArr){if(client.url===url && 'focus' in client) return client.focus();}
-      if(clients.openWindow) return clients.openWindow(url);
-    })
-  );
+  event.waitUntil(clients.openWindow('/'));
 });
